1fa9e63374ac94ba177c79720064518145f2c98b
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-03-22 16:25:21 -0300

    Atualizar o index.py

--- exercicios/index.py
+++ exercicios/index.py
@@ -2,3 +2,9 @@ def menu (msg):
     print('*' * len(msg))
     print(msg)
     print('*' * len(msg))
+
+def fatorial (num):
+    if num <= 1:
+       return 1
+    else:
+       return num * fatorial(num -= 1)
